9099189c3a8468fa9e324eb2abcfdb3bccf6d79c
Ernest Eferetin <ernesteferetin@yahoo.com>
2025-10-25 12:16:28 +0100

    Updated

--- app/components/GetTheApp.tsx
+++ app/components/GetTheApp.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 export default function GetTheApp(){
-    return(  <div className="bg-[#F8F9FF] px-[16px] py-[60px] md:px-[55px] g-4 lg:gap-1">
-        <div className="flex flex-col lg:flex-row py-[1px] md:p-[0px] bg-[#6F87F8] rounded-[20px] items-center">
+    return(  <div className="bg-[#F8F9FF] px-[16px] py-[60px] md:px-[55px] g-4 lg:gap-1 ">
+        <div className="flex flex-col lg:flex-row  bg-[#6F87F8] rounded-[20px] items-center gap-4 ">
             <div className="flex flex-col pl-[60px]">
               <span className="font-semibold text-[40px] text-white">Get the App</span>
               <span className="text-white font-normal text-[16px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</span>
@@ -11,7 +11,7 @@ export default function GetTheApp(){
                 alt="Image"
                 height={434}
                 width={580}
-                className="h-[200px] w-[250px] md:h-[434px] md:w-[580px]"
+                className="h-[200px] w-[250px] md:h-[424px] md:w-[580px] h-full"
             />
         </div>
 </div>)
